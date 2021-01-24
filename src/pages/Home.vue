5a23508b3794195815dc97e81638439295f57531
Jakub <jakubsliz03@gmail.com>
2021-01-24 20:11:33 +0100

    add main with header

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,17 @@
 <template>
-  <header class="flex flex-wrap items-center justify-between px-12 h-32">
-    <ul class="order-last md:order-first flex-1 flex justify-center md:justify-start list-reset">
-				<li>
-					<a href="#" class="text-grey-darker no-underline hover:text-black">kuba</a>
-				</li>
-				<li class="ml-8">
-					<a href="#" class="text-grey-darker no-underline hover:text-black">kuba</a>
-				</li>
-				<li class="ml-8">
-					<a href="#" class="text-grey-darker no-underline hover:text-black">kuba</a>
-				</li>
-			</ul>
-	  <div class="flex lg:justify-end">
-				<a href="#" class="block w-4 h-2 border-t-2 border-b-2 border-black hover:border-gray-600"></a>
+  <main>
+	<header class="relative z-40 w-full h-24 px-12 flex justify-between items-center">
+	
+		<div class="flex justify-center lg:justify-start">
+			<a href="#" class="block w-4 h-2 border-t-2 border-b-2 border-black hover:border-gray-600"></a>
 		</div>
-  </header>
+
+		<div class="flex flex-1 lg:justify-center">
+			<a href="#" class="font-serif text-2xl block text-center text-black no-underline hover:text-gray-600">Capoeira</a>
+		</div>
+
+	</header>	
+  </main>
 </template>
 
 <script>
