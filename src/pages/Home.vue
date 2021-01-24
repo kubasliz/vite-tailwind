8c0df93e6510a0602a984a8a5adcf7e0a148f421
Kuba <jakubsliz03@gmail.com>
2021-01-24 19:05:06 +0100

    lead-up for the project

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,27 +2,29 @@
   <header class="flex flex-wrap items-center justify-between px-12 h-32">
     <ul class="order-last md:order-first flex-1 flex justify-center md:justify-start list-reset">
 				<li>
-					<a href="#" class="text-sm text-grey-darker no-underline hover:text-black">kuba</a>
+					<a href="#" class="text-grey-darker no-underline hover:text-black">kuba</a>
 				</li>
 				<li class="ml-8">
-					<a href="#" class="text-sm text-grey-darker no-underline hover:text-black">kuba</a>
+					<a href="#" class="text-grey-darker no-underline hover:text-black">kuba</a>
 				</li>
 				<li class="ml-8">
-					<a href="#" class="text-sm text-grey-darker no-underline hover:text-black">kuba</a>
+					<a href="#" class="text-grey-darker no-underline hover:text-black">kuba</a>
 				</li>
 			</ul>
+	  <div class="flex lg:justify-end">
+				<a href="#" class="block w-4 h-2 border-t-2 border-b-2 border-black hover:border-gray-600"></a>
+		</div>
   </header>
 </template>
 
 <script>
-import { onMounted } from 'vue'
-import MyHeader from '../components/MyHeader.vue'
+import { watchEffect } from 'vue'
+
 export default {
   components: {
-    MyHeader
   },
   setup () {
-    onMounted(() => {
+    watchEffect(() => {
       console.log('yolo')
     })
     return {}
