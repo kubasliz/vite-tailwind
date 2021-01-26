176c07ed533af786bd204b8e9bdba646ffcc5ce5
Jakub <jakubsliz03@gmail.com>
2021-01-26 21:48:51 +0100

    add new section

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -15,6 +15,31 @@
 	</header>	
 
   </main>
+
+  <div class="w-full py-24 px-6 relative z-10">
+		
+	  <div class="max-w-4xl mx-auto text-left flex items-center">
+
+		  <div class="w-full">
+        <span class="text-gray">Introduction</span>
+        <span class="inline-block h-1 w-24 ml-4 border-t border-gray-600"></span>
+			  <h1 class="text-2xl text-gray-900 mb-3">Sit id reprehenderit adipisicing elit ad nisi irure ea cillum.</h1>
+				  <p class="text-md text-gray-600 leading-normal">
+						Quis cupidatat et velit minim laborum magna cupidatat laboris enim sunt fugiat.Ipsum eu aute aliqua incididunt.Culpa aliqua mollit fugiat ut nisi exercitation ut irure.
+					</p>
+
+			</div>
+
+			<div class="hidden md:block w-1/3 mr-20">
+        <a href="#" class="block w-44 h-44 rounded-full border-2 overflow-hidden">
+				  <img src="https://images.unsplash.com/photo-1492567291473-fe3dfc175b45?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=631&q=80" class="w-full h-full object-cover">
+				</a>
+			</div>
+		
+    </div>
+
+	</div>
+
 </template>
 
 <script>
@@ -35,7 +60,7 @@ export default {
 <style>
 
   #img {
-    clip-path: polygon(100% 0, 100% 83%, 19% 101%, 0 79%, 0 0);
+    clip-path: polygon(100% 0, 100% 83%, 19% 100%, 0 79%, 0 0);
   }
 
 </style>
